6fcd05421ea5f5837d03d6aa9e2fd9f5e1c54598
Hussein Kabbout <hussein.kabbout@sourcepole.ch>
2018-04-09 12:57:56 +0200

    commented import out

--- pyqtconfig/qt.py
+++ pyqtconfig/qt.py
@@ -44,7 +44,7 @@ if USE_QT_PY == PYQT5:
     from PyQt5.QtWebKit import *
     from PyQt5.QtNetwork import *
     from PyQt5.QtWidgets import *
-    from PyQt5.QtWebKitWidgets import *
+    # from PyQt5.QtWebKitWidgets import *
 
 elif USE_QT_PY == PYSIDE:
     from PySide.QtGui import *
