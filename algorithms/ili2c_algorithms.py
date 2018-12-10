a155ef617f62665d25311f20cb2a7e18402f56b3
lindauju <45693873+lindauju@users.noreply.github.com>
2018-12-10 09:06:28 +0100

    Update ili2c_algorithms.py
    Avoid failure when opening Interlis plugin. Change load QCoreApplication from qgis.core to qgis.PyQt.QtCore.

--- algorithms/ili2c_algorithms.py
+++ algorithms/ili2c_algorithms.py
@@ -29,10 +29,10 @@ __copyright__ = '(C) 2016 by Pirmin Kalberer'
 
 __revision__ = '$Format:%H$'
 
+from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (QgsProcessingAlgorithm,
                        QgsProcessingParameterFile,
-                       QgsProcessingParameterFileDestination,
-                       QCoreApplication
+                       QgsProcessingParameterFileDestination
                        )
 
 from .interlis_utils import IliUtils
